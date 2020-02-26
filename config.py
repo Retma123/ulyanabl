999170a1c74a8b7ca4bcb86b846f2945bbf38e3b
Retma123 <60816157+Retma123@users.noreply.github.com>
2020-02-26 22:54:20 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = 'NjgwNzMzNjE0MjY2MTIyMzI1.XlbEGg._avqbcbUiNa0lL0cRl6pYXPF3QM' # bot token
+
+POST_ID = 680835521991540821 # post id to read reaxtions from
+
+# roles list according to emotes
+ROLES = {
+	'🐉': 681040574396366858,
+	'🔥': 680834732321538149,
+	'🧊': 680834994612338835,
+}
+
+#exclude this roles from counting
+EXCROLES = () 
+
+MAX_ROLES_PER_USER = 999 # max ammount of roles a user can have
